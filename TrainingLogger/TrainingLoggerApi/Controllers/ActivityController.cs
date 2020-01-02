41c3e4b4911a7ee147f6991e53f2123028a82dd7
Keith Babinec <keith.babinec@gmail.com>
2020-01-01 20:20:18 -0800

    Added solution file, empty web api and shared lib projects

---
+++ TrainingLogger/TrainingLoggerApi/Controllers/ActivityController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace TrainingLoggerApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ActivityController : ControllerBase
+    {
+        private readonly ILogger<ActivityController> _logger;
+
+        public ActivityController(ILogger<ActivityController> logger)
+        {
+            _logger = logger;
+        }
+    }
+}
---
+++ TrainingLogger/TrainingLoggerSharedLibrary/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TrainingLoggerSharedLibrary
+{
+    public class Class1
+    {
+    }
+}
