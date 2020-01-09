7418ef36664ea57d4f5fe05c02ea1891c956b305
Keith Babinec <keith.babinec@gmail.com>
2020-01-09 00:11:16 -0800

    Added app settings service support for new base URI setting

--- TrainingLogger/TrainingLoggerWeb/src/Services/AppSettingsService.js
+++ TrainingLogger/TrainingLoggerWeb/src/Services/AppSettingsService.js
@@ -8,6 +8,9 @@ class AppSettingsService {
     GetBrowserCacheLocation() {
         return process.env.REACT_APP_BROWSER_CACHE_LOCATION;
     }
+    GetWebApiUri() {
+        return process.env.REACT_APP_WEB_API_BASE_URI;
+    }
 }
 
 export default AppSettingsService;
