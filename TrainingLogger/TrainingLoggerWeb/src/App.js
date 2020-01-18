69eb622c6e64b2cc63d37f283dd588d57b45fbe8
Keith Babinec <keith.babinec@gmail.com>
2020-01-17 23:10:44 -0800

    Added react router and basic routes

--- TrainingLogger/TrainingLoggerWeb/src/App.js
+++ TrainingLogger/TrainingLoggerWeb/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
+        <Router>
+          <Switch>
+            <Route exact path="/">
+
+            </Route>
+            <Route exact path="/new-activity">
+
+            </Route>
+            <Route exact path="/reporting">
+
+            </Route>
+          </Switch>
+        </Router>
       </div>
     );
   }
