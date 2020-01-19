a083296f9648f78acc1e27a1a3ceb5a11ac9bea4
Keith Babinec <keith.babinec@gmail.com>
2020-01-19 12:42:17 -0800

    Added navbar w/ links to primary views

--- TrainingLogger/TrainingLoggerWeb/src/App.js
+++ TrainingLogger/TrainingLoggerWeb/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import NewActivity from './Components/NewActivity/NewActivity';
 import Reporting from './Components/Reporting/Reporting';
@@ -10,17 +10,37 @@ class App extends React.Component {
     return (
       <div className="App">
         <Router>
-          <Switch>
-            <Route path="/">
-              <Home />
-            </Route>
-            <Route path="/new-activity">
-              <NewActivity />
-            </Route>
-            <Route path="/reporting">
-              <Reporting />
-            </Route>
-          </Switch>
+          <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+              <ul className="navbar-nav">
+                <li className="nav-item">
+                  <Link to="/" className="nav-link">Home</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/new-activity" className="nav-link">New Activity</Link>
+                </li>
+                <li className="nav-item">
+                  <Link to="/reporting" className="nav-link">Reporting</Link>
+                </li>
+              </ul>
+            </div>
+          </nav>
+          <div>
+            <Switch>
+              <Route path="/new-activity">
+                <NewActivity />
+              </Route>
+              <Route path="/reporting">
+                <Reporting />
+              </Route>
+              <Route path="/">
+                <Home />
+              </Route>
+            </Switch>
+          </div>
         </Router>
       </div>
     );
