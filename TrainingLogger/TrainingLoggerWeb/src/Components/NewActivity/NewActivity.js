5c4557a1664bbad1b45354ffe909ca77ceb6a2de
Keith Babinec <keith.babinec@gmail.com>
2020-01-21 23:25:23 -0800

    Added new activity form styling/layout

--- TrainingLogger/TrainingLoggerWeb/src/Components/NewActivity/NewActivity.js
+++ TrainingLogger/TrainingLoggerWeb/src/Components/NewActivity/NewActivity.js
@@ -4,68 +4,90 @@ import './NewActivity.css';
 class NewActivity extends React.Component {
   render() {
     return (
-      <div className="NewActivity">
-        <form>
-          <div class="form-group">
-            <label for="dateSelection">Date</label>
-            <input type="text" class="form-control" id="dateSelection" />
+      <div className="New-Activity-Component-Root">
+        <h1 className="display-4 text-left">New Activity</h1>
+        <form className="New-Activity-Submission-Form">
+          <div className="form-group row">
+            <label htmlFor="dateSelection" className="col-sm-2 col-form-label col-form-label-sm">Date</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="dateSelection" />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="activitySelectField">Activity</label>
-            <select class="form-control" id="activitySelectField">
-              <option>Run</option>
-              <option>Hike</option>
-              <option>Cycle</option>
-            </select>
+          <div className="form-group row">
+            <label htmlFor="activitySelectField" className="col-sm-2 col-form-label col-form-label-sm">Activity</label>
+            <div className="col-sm-10">
+              <select className="form-control form-control-sm" id="activitySelectField">
+                <option>Run</option>
+                <option>Hike</option>
+                <option>Cycle</option>
+              </select>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="purposeSelectField">Purpose</label>
-            <select class="form-control" id="purposeSelectField">
-              <option>Training</option>
-              <option>Race</option>
-              <option>Leasure</option>
-            </select>
+          <div className="form-group row">
+            <label htmlFor="purposeSelectField" className="col-sm-2 col-form-label col-form-label-sm">Purpose</label>
+            <div className="col-sm-10">
+              <select className="form-control form-control-sm" id="purposeSelectField">
+                <option>Training</option>
+                <option>Race</option>
+                <option>Leasure</option>
+              </select>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="surfaceSelectField">Surface Type</label>
-            <select class="form-control" id="surfaceSelectField">
-              <option>Road</option>
-              <option>Trail</option>
-              <option>Track</option>
-              <option>Treadmill</option>
-              <option>Stepper</option>
-            </select>
+          <div className="form-group row">
+            <label htmlFor="surfaceSelectField" className="col-sm-2 col-form-label col-form-label-sm">Surface</label>
+            <div className="col-sm-10">
+              <select className="form-control form-control-sm" id="surfaceSelectField">
+                <option>Road</option>
+                <option>Trail</option>
+                <option>Track</option>
+                <option>Treadmill</option>
+                <option>Stepper</option>
+              </select>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="durationSelection">Duration</label>
-            <input type="text" class="form-control" id="durationSelection" />
+          <div className="form-group row">
+            <label htmlFor="durationSelection" className="col-sm-2 col-form-label col-form-label-sm">Duration</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="durationSelection" />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="distanceSelection">Distance</label>
-            <input type="text" class="form-control" id="distanceSelection" />
+          <div className="form-group row">
+            <label htmlFor="distanceSelection" className="col-sm-2 col-form-label col-form-label-sm">Distance</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="distanceSelection" />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="averageIntensityField">Intensity (Average)</label>
-            <select class="form-control" id="averageIntensityField">
-              <option>Zone1</option>
-              <option>Zone2</option>
-              <option>Zone3</option>
-              <option>Zone4</option>
-              <option>Zone5</option>
-            </select>
+          <div className="form-group row">
+            <label htmlFor="averageIntensityField" className="col-sm-2 col-form-label col-form-label-sm">Intensity</label>
+            <div className="col-sm-10">
+              <select className="form-control form-control-sm" id="averageIntensityField">
+                <option>Zone1</option>
+                <option>Zone2</option>
+                <option>Zone3</option>
+                <option>Zone4</option>
+                <option>Zone5</option>
+              </select>
+            </div>
           </div>
-          <div class="form-group">
-            <label for="elevationGainSelection">Elevation Gain</label>
-            <input type="text" class="form-control" id="elevationGainSelection" />
+          <div className="form-group row">
+            <label htmlFor="elevationGainSelection" className="col-sm-2 col-form-label col-form-label-sm">Gain</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="elevationGainSelection" />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="elevationLossSelection">Elevation Loss</label>
-            <input type="text" class="form-control" id="elevationLossSelection" />
+          <div className="form-group row">
+            <label htmlFor="elevationLossSelection" className="col-sm-2 col-form-label col-form-label-sm">Loss</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control form-control-sm" id="elevationLossSelection" />
+            </div>
           </div>
-          <div class="form-group">
-            <label for="notesSelection">Optional Notes</label>
-            <textarea class="form-control" id="notesSelection" rows="3"></textarea>
+          <div className="form-group row">
+            <label htmlFor="notesSelection" className="col-sm-2 col-form-label col-form-label-sm">Notes</label>
+            <div className="col-sm-10">
+              <textarea className="form-control form-control-sm" id="notesSelection" rows="3"></textarea>
+            </div>
           </div>
+          <button className="btn btn-primary New-Activity-Submit-Button" type="submit">Submit</button>
         </form>
       </div>
     );
