95736b0e65791afad51795fc9b14a2f253123191
Keith Babinec <keith.babinec@gmail.com>
2020-02-01 12:49:03 -0800

    Added missing orderby clause in activities query

--- TrainingLogger/TrainingLoggerDb/StoredProcedures/GetActivitiesByUser.sql
+++ TrainingLogger/TrainingLoggerDb/StoredProcedures/GetActivitiesByUser.sql
@@ -21,20 +21,21 @@ BEGIN
 		
 		BEGIN TRANSACTION
 		
-		SELECT	[dbo].[Activities].[ID],
-				[dbo].[Activities].[UserObjectId],
-				[dbo].[Activities].[Date],
-				[dbo].[Activities].[Type],
-				[dbo].[Activities].[Purpose],
-				[dbo].[Activities].[Surface],
-				[dbo].[Activities].[Duration],
-				[dbo].[Activities].[DistanceInMeters],
-				[dbo].[Activities].[AverageIntensity],
-				[dbo].[Activities].[ElevationGain],
-				[dbo].[Activities].[ElevationLoss],
-				[dbo].[Activities].[Notes]
-		FROM	[dbo].[Activities]
-		WHERE	[dbo].[Activities].[UserObjectId] = @UserObjectId
+		SELECT		[dbo].[Activities].[ID],
+					[dbo].[Activities].[UserObjectId],
+					[dbo].[Activities].[Date],
+					[dbo].[Activities].[Type],
+					[dbo].[Activities].[Purpose],
+					[dbo].[Activities].[Surface],
+					[dbo].[Activities].[Duration],
+					[dbo].[Activities].[DistanceInMeters],
+					[dbo].[Activities].[AverageIntensity],
+					[dbo].[Activities].[ElevationGain],
+					[dbo].[Activities].[ElevationLoss],
+					[dbo].[Activities].[Notes]
+		FROM		[dbo].[Activities]
+		WHERE		[dbo].[Activities].[UserObjectId] = @UserObjectId
+		ORDER BY	[dbo].[Activities].[Date] DESC
 
 		COMMIT TRANSACTION
 
